2fc62ad92ed17a04ee9963ca666b94a90ae0bb5e
Mr.Nova@9810 <surajduse@gmail.com>
2026-07-17 17:32:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ping-Pong
+first game I tried to make with the help of tutorial, In java, it's a fun game to play with friends, together.
---
+++ src/pongGame.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+//awt (abstract windows toolkit) -> used for creating Graphical user interface
+import java.awt.event.*;
+// used for doing some action by using input changes like mouse ,keyboard , etc.
+import java.util.*;
+import javax.swing.*;
+
+public class pongGame {
+    public static void main(String[] args){
+
+         GameFrame frame1 = new GameFrame();
+    }
+}
